4f72b995ce5f7d50c61b68119301981b2b596307
abdelrhman892 <abdoayman@gmail.com>
2023-11-28 11:28:41 -0800

    yarb

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+num = 0.0
+deletedNum = 1.0
+while int(num) <= 89:
+    lastDigit = int(repr(int(num))[-1])
+    if num < 89.0:
+        if (lastDigit == 0) and (num > 9):
+            deletedNum += 1.0
+            num += deletedNum
+        print("{:02}, ".format(int(num)), end="")
+        num += 1
+    else:
+        if lastDigit == 9:
+            deletedNum += 1.0
+            num += deletedNum
+        print("{:02}".format(int(num)))
+        num += 1

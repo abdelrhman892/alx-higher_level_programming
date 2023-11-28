813cf60afd1b9977283ae2cea6791b25c076b254
abdelrhman892 <abdoayman@gmail.com>
2023-11-28 06:18:38 -0800

    ss

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,3 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98  # LOAD_CONST 1 (98)
-    result += a   # LOAD_FAST a
-    result = result ** b  # BINARY_POWER
-    result += a + b  # BINARY_ADD
-    return result  # RETURN_VALUE
-
+   return 98 + a**b

bedfaf4004156d554c070446ebec60c3474fcd05
abdelrhman892 <abdoayman@gmail.com>
2023-12-03 15:53:51 -0800

    ss

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,20 +10,20 @@ int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
 		return (1);
-	return (aux_palind(head, *head));
+	return (crestuse(head, *head));
 }
 
 /**
- * aux_palind - funct to know if is palindrome
+ * crestuse - funct to know if is palindrome
  * @head: head list
  * @end: end list
  * Return: 1 or 0
 */
-int aux_palind(listint_t **head, listint_t *end)
+int crestuse(listint_t **head, listint_t *end)
 {
 	if (end == NULL)
 		return (1);
-	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	if (crestuse(head, end->next) && (*head)->n == end->n)
 	{
 		*head = (*head)->next;
 		return (1);

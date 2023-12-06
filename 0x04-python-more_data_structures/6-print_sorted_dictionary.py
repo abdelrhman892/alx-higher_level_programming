0e61eaa0ac209b5a67def4ce2df2179671a08687
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2023-12-06 15:24:29 +0200

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,7 @@ def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
 
     for key in sorted_keys:
-        print(f"{key}: {a_dictionary[key]}")
-
+        if isinstance(key, str):
+            print(f"{key}: {a_dictionary[key]}")
+        else:
+            return None

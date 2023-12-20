6b002efed960575dc4f752fd12129a4068a0c1a2
abdelrhman892 <abdoayman5m@gmail.com>
2023-12-19 16:14:18 -0800

    fixing

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,13 @@
 class Square:
     """anything"""
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
+        """constractur"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
         self.position = position
 
     @property

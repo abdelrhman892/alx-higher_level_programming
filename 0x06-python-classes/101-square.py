73ed4959ece83fc88a1092e5446a9ecb635013b3
abdulrhman <115238439+abdelrhman892@users.noreply.github.com>
2023-12-20 03:35:20 +0200

    Update 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -55,7 +55,7 @@ class Square:
             [print("#", end="") for _ in range(0, self.__size)]
             print("")
 
-    def str(self):
+    def __str__(self):
         """Define the print () representation of a Square."""
         if self.__size != 0:
             [print("") for _ in range(0, self.__position[1])]

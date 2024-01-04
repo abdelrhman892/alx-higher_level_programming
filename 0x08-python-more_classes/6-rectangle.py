b06d3a832405eb37571ed316d34fa2d557d49602
abdulrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-01-04 20:59:31 +0200

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -65,5 +65,5 @@ class Rectangle:
 
     def __del__(self):
         """nothing"""
-        Rectangle.number_of_instances -= 1
         print('Bye rectangle...')
+        Rectangle.number_of_instances -= 1

598528e0482dffcba8077456fcb21f4f405fb8be
abdelrhman892 <abdoayman5m@gmail.com>
2024-01-08 15:49:13 -0800

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+nothing
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """None"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False

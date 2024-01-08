72d27442642fbacf193ad042474b7256ad56ea0d
abdulrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-01-09 01:55:38 +0200

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ nothing
 
 def inherits_from(obj, a_class):
     """None"""
-    if isinstance(obj, a_class):
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False

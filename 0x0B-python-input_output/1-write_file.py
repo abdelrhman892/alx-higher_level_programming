912d81369848ad02e4b1eacb75ff98904dd53d44
abdulrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-01-09 16:45:20 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,8 +5,7 @@
 def write_file(filename="", text=""):
     """None"""
     numOfChar = 0
-    with open(filename, mode= 'w', encoding= 'utf-8') as file:
+    with open(filename, mode='w', encoding='utf-8') as file:
         file.write(text)
-        file.close()
 
     return len(text)

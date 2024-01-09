75f86cc0a738b675ac438143cba3fa0ff044eb8a
abdulrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-01-09 23:31:43 +0200

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,11 +7,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    while len(triangle) != n:
-        tri = triangle[-1]
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
-            tmp.append(1)
-            triangle.append(tmp)
-    return triangle
+            tmp.append(tri[1] + tri[i + 1])
+        triangles.append(1)
+        triangles.append(tmp)
+    return triangles

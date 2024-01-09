bfa400e26342f4e1d83943dc3daf1d790a9283ae
abdelrhman892 <abdoayman5m@gmail.com>
2024-01-09 07:22:36 -0800

    ss

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""None"""
+
+
+def append_write(filename="", text=""):
+    """Appends text to"""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return file.write(text)

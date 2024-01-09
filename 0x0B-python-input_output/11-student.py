bddb2768b15ca13fb9b1059f5c82598778d9f95e
abdelrhman892 <abdoayman5m@gmail.com>
2024-01-09 13:07:36 -0800

    ss

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Library for"""
+
+class Student:
+    """None"""
+
+    def __init__(self, first_name, last_name, age):
+        """None"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """None"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dick = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dick[key] = value
+        return my_dick
+
+    def reload_from_json(self, json):
+        """None"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value

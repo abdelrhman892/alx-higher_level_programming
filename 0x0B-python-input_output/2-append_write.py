b41c3c1db61cefacebb811968e205d1bf0bf5b7f
abdulrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-01-09 17:24:51 +0200

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
     """Appends text to"""
-    with open(filename, mode='w', encoding='utf-8') as file:
+    with open(filename, mode='a', encoding='utf-8') as file:
         return file.write(text)

a0e57b30fd872f013d8b947534cf5e120db1a8f6
abdelrhman892 <abdoayman5m@gmail.com>
2024-01-12 06:22:27 -0800

    SquareClass

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+"""
+    This class will be the Square
+    to clac all things about Square
+"""
+
+
+class Square(Rectangle):
+    """Represents a Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value

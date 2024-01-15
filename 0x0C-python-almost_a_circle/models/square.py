996faad6043d85d90ec65c0f0778bc51f15c3b62
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-01-15 15:22:46 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,30 +31,6 @@ class Square(Rectangle):
                              'x': self.x, 'y': self.y}
 
         for key, value in kwargs.items():
-            if key in attribute_mapping:class Square(Rectangle):
-    """Represents a Square"""
-
-    def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        return (f"[{Square.__name__}] ({self.id}) "
-                f"{self.x}/{self.y} - {self.width}")
-
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
-
-    def update(self, *args, **kwargs):
-        attribute_mapping = {'id': self.id, 'size': self.size,
-                             'x': self.x, 'y': self.y}
-
-        for key, value in kwargs.items():
             if key in attribute_mapping:
                 setattr(self, key, value)
 
@@ -64,8 +40,3 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
-                setattr(self, key, value)
-
-        for arg, key in enumerate(('id', 'size', 'x', 'y')):
-            if arg < len(args):
-                setattr(self, key, args[arg])

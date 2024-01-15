20e2ef85279672112bc4ade062f2211f284421fe
abdelrhman892 <abdoayman5m@gmail.com>
2024-01-15 04:28:23 -0800

    update the square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,7 +14,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[{Square.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
@@ -24,3 +24,32 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        # for key, value in kwargs.items():
+        #     if key == 'id':
+        #         self.id = value
+        #     if key == 'size':
+        #         self.size = value
+        #     if key == 'x':
+        #         self.x = value
+        #     if key == 'y':
+        #         self.y = value
+        # for arg in range(len(args)):
+        #     if arg == 0:
+        #         self.id = args[arg]
+        #     if arg == 1:
+        #         self.size = args[arg]
+        #     if arg == 2:
+        #         self.x = args[arg]
+        #     if arg == 3:
+        #         self.y = args[arg]
+        attribute_mapping = {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+
+        for key, value in kwargs.items():
+            if key in attribute_mapping:
+                setattr(self, key, value)
+
+        for arg, key in enumerate(('id', 'size', 'x', 'y')):
+            if arg < len(args):
+                setattr(self, key, args[arg])

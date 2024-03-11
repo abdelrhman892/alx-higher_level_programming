36ecc329fe1e5e4bf8fcdd6c81df97d6f5eae39f
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-11 16:15:04 +0200

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function add(a, b) {
-    if (a === undefined || b == undefined) {
-        console.log('NaN');
-    }
-    else {
-        console.log(Number(a) + Number(b));
-    }
+function add (a, b) {
+  if (a === undefined || b === undefined) {
+    console.log('NaN');
+  }
+  else {
+    console.log(Number(a) + Number(b));
+  }
 }
 add(process.argv[2], process.argv[3]);

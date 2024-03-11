38e41913c73d73c347c84db76749671973ca4937
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-11 13:48:46 +0200

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-if (!arg1 || !arg2) {
-  console.log('No argument');
+// if (!arg1 || !arg2) {
+//   console.log('No argument');
+// } else {
+//   console.log(process.argv[2]);
+// }
+const [arg] = process.argv.slice(2);
+
+if (arg === undefined) {
+  console.log("No argument");
 } else {
-  console.log(process.argv[2]);
+  console.log(arg);
 }

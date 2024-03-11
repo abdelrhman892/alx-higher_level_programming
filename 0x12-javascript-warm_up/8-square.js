b012e2f6208fd5b1b46545b47b448ef3ba0435ea
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-11 15:40:50 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,10 @@ if (process.argv.length === 2 || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < Number(process.argv[2]); i++) {
-      let output = '';
-      for (let y = 0; y < Number(process.argv[2]); y++) {
-          output += 'x';
-      }
-      console.log(output);
+    let output = '';
+    for (let y = 0; y < Number(process.argv[2]); y++) {
+      output += 'x';
+    }
+    console.log(output);
   }
 }

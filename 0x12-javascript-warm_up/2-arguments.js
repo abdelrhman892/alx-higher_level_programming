5d13a25dddbfc75df85ad17d4a6bf58e27213dbd
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-11 13:35:50 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
   console.log('No argument');
+  return
 }
 else if (process.argv.length === 3) {
   console.log('Argument found');
+  return
 }
 else {
   console.log('Arguments found');
+  return
 }

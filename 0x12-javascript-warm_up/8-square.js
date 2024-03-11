585b7c1e5eefff4d8a30ed6d7f1352dac1d27765
abdelrhman892 <abdoayman5m@gmail.com>
2024-03-11 06:38:20 -0700

    s

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (process.argv.length === 2 || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < Number(process.argv[2]); i++) {
+      let output = '';
+      for (let y = 0; y < Number(process.argv[2]); y++) {
+          output += 'x';
+      }
+      console.log(output);
+  }
+}

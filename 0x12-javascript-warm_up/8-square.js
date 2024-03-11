f8a0bb0b9aa8a7a6219bd75bb7eaee2ee7064839
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-11 15:45:10 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (process.argv.length === 2 || isNaN(process.argv[2])) {
   for (let i = 0; i < Number(process.argv[2]); i++) {
     let output = '';
     for (let y = 0; y < Number(process.argv[2]); y++) {
-      output += 'x';
+      output += 'X';
     }
     console.log(output);
   }

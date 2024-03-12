8d1ff40451a56ab6fc18b49b1e79a1f10fff1b59
abdelrhman892 <abdoayman5m@gmail.com>
2024-03-12 07:29:26 -0700

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        let output = '';
+        for (let y = 0; y < this.width; y++) {
+          output += c;
+        }
+        console.log(output);
+      }
+    }
+  }
+}
+
+module.exports = Square;

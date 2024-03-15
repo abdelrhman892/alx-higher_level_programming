ad370833653993e0865c661884129fb24523499f
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-15 14:09:25 +0200

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
 import MySQLdb
+import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3],

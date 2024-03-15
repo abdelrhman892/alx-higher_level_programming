9510b1e5511450d8e8903825bd42ecf01d6e51dd
abdelrhman892 <abdoayman5m@gmail.com>
2024-03-15 04:23:55 -0700

    fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
-import sys
 import MySQLdb
+import sys
 
-conn = MySQLdb.connect(
-    host="localhost",
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    database=sys.argv[3]
-)
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-for state in cursor.fetchall():
-    print(state)
+if __name__ == "__main__":
+    # Check if correct number of arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
-cursor.close()
-conn.close()
+    # Connect to MySQL server
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cursor = db.cursor()
+
+        # Execute the query to select all states
+        query = "SELECT * FROM states ORDER BY id ASC"
+        cursor.execute(query)
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+        # Close cursor and database connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)

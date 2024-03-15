be4c7a606bdab0102a30f7b7bf7b0e8dd3398beb
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-15 13:45:20 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,17 +3,16 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Connect to MySQL server
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database)
         cursor = db.cursor()
 
         # Execute the query to select all states
@@ -30,7 +29,3 @@ if __name__ == "__main__":
         # Close cursor and database connection
         cursor.close()
         db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)

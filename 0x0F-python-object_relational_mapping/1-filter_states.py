1d84f6f3297ed27579e565300c13875608fc944b
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-15 14:08:04 +0200

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE id > 3 ORDER BY states.id""")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
     for state in rows:
         print(state)

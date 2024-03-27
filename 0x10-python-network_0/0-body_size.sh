d24c9a00c7001d432f2a5d595997eae92afc1842
abdelrhman892 <abdoayman5m@gmail.com>
2024-03-27 15:00:31 -0700

    s

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request using curl and get the size of the response body in bytes
-size=$(curl -sI "$1" | awk '/Content-Length/ {print $2}')
-
-# Display the size of the response body in bytes
-echo "$size"
+# Show size of body of response
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f 2

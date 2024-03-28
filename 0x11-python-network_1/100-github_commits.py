c643ad226ee5b24a2440968d141cc58a1525c7f4
abdelrhman892 <abdoayman5m@gmail.com>
2024-03-28 06:48:46 -0700

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+takes 2 arguments in order to solve this challenge
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    req = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(argv[2], argv[1]))
+    commits = req.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))

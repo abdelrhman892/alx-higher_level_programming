ba28b7c6edb656aade68a28cc544421d9abe2d17
abdelrhman892 <abdoayman5m@gmail.com>
2024-03-28 06:24:19 -0700

    s

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,7 +10,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    URL = argv[1]
-    pay_load = {'email': argv[2]}
-    req = requests.post(URL, data=pay_load)
-    print(req.text)
+    url = argv[1]
+    payload = {'email': argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)

43f40654eb6e7d0361632a33a22c6bbf6dd65ff0
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-07-04 19:40:30 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,21 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log('0');
-} else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+const args = process.argv.slice(2);
+let arrOfNum = []
+const num = parseInt(args, 10);
+let bigNum = 0;
+if (args.length < 1){
+    console.log(0);
+} else if (args.length === 1 && num === 1){
+    console.log(0);
+}
+else {
+    const arr = []
+    for (let i of args){
+        const number = parseInt(i, 10);
+        arr.push(number);
+    }
+    arr.sort()
+    arrOfNum = [...new Set(arr)];
+    console.log(arrOfNum)
+    console.log(arrOfNum[arrOfNum.length - 2])
 }

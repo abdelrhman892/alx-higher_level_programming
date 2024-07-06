ce4a7cd5f56237f73468fcf860103d6680849566
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-07-06 17:16:56 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
 
-// Import the 'request' module
 const request = require('request');
+let count = 0;
+let argv = process.argv.splice(2);
 
-// Use the 'request' module to perform an HTTP GET request to the URL
-request(process.argv[2], function (error, response, body) {
-  // Check if there was no error during the HTTP request.
-  if (!error) {
-    // parse the JSON data and extract the "results" array
-    const results = JSON.parse(body).results;
-    // Use the 'reduce()' method to iterate through the movies in the 'results' array.
-    console.log(results.reduce((count, movie) => {
-      // check if there is a character with ID 18 ('/18/') in the 'characters' array.
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        // If a character with ID 18 is found, increment the count by 1.
-        ? count + 1
-        // Otherwise, keep the count unchanged.
-        : count;
-      // The 'reduce()' method starts with an initial value of 0 ('0' at the end).
-    }, 0));
-  }
-});
+if (argv.length < 1) {
+    console.error("There is no movie ID");
+} else {
+    request(argv[0], (error, response, body) => {
+        if (error) {
+            console.error("Error: ", error);
+        } else {
+           let bodyToObject = JSON.parse(body);
+           for (let key of bodyToObject.results) {
+                for (let i of key["characters"]) {
+                    let arr = i.split("/");
+                    if (arr[arr.length - 2] === "18") {
+                        count++;
+                    }
+                }
+           }
+           console.log(count)
+        }
+    })
+}
